afabfb4433f6b9fc986a57bb50b05fa586dc0ba1
anikethana2109-maker <anikethana2109@gmail.com>
2026-04-09 07:19:53 +0530

    first commit

--- pro2/src/App.jsx
+++ pro2/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import {Bookmark} from 'lucide-react'
+import React from 'react'
+import { Bookmark } from 'lucide-react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="parent">
+    <div className='parent'>
       <div className="card">
         <div className="top">
-          <img src = "https://stockunlock.com/stockDetails/AMZN/general" alt="amzn logo" />
-          <button className="bookmark"> save<Bookmark /></button>
+          <img src="https://static2.finnhub.io/file/publicdatany/finnhubimage/stock_logo/AMZN.svg" alt="Amazon" />
+          <button className="save-btn">
+            Save <Bookmark size={16} />
+          </button>
+        </div>
+        <div className="center">
+          <h1>Amazon</h1>
+          <p>Global e-commerce leader.</p>
         </div>
       </div>
     </div>
